82896fedaf13b37d2950217a072830a0b57e8323
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-10-02 10:18:07 -0300

    br1 commit

---
+++ src/Aula3.1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Ola tudo bem!" << std::endl;
+    return 0;
+}
+
---
+++ src/Aula3.2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	//Imprimir o basico
+	std::cout << "Seu Nome Completo" << std ::endl;
+	std::cout << "Nome da Disciplina" << std::endl;
+	std::cout << "Nome da instituicao" << std::endl;
+	return 0;
+}
