f1ebc3e8bb43673210b5ef52c6502fbc7dc2670a
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-10-03 10:05:12 -0300

    8.1th commit

--- src/Aula3.3.cpp
+++ src/Aula3.3.cpp
@@ -3,12 +3,12 @@
 int main () {
 	//Frases assasinas
 	std::cout << "Isto nao vai dar certo." << std::endl;
-	std::cout << "Voc� nunca vai conseguir." << std::endl;
-	std::cout << "Voc� vai se estrepar." << std::endl;
-	std::cout << "N�o vai dar em nada." << std::endl;
-	std::cout << "Est� tudo errado!" << std::endl;
+	std::cout << "Voce nunca vai conseguir." << std::endl;
+	std::cout << "Voce vai se estrepar." << std::endl;
+	std::cout << "Nao vai dar em nada." << std::endl;
+	std::cout << "Esta tudo errado!" << std::endl;
 	//Adicione suas proprias frases
 	std::cout << "Voce vai fracassar." << std::endl;
-	std::cout << "Isso � uma perda de tempo." << std::endl;
+	std::cout << "Isso e uma perda de tempo." << std::endl;
 	return 0;
 }
